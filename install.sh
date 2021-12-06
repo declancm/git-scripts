e54c5dd91cab6b4c64d5d686bc5d7233763eef5d
declancm <declanmullen3.14@gmail.com>
2021-12-06 03:07:50 -0800

    Added sub-directory for installation.

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@ printf "Do you want to install for vim or neovim?"
 read input
 if [ $input == 'vim' || $input == 'Vim' ]
 then
-    cp ./git.sh ~/git.sh
-    printf "nnoremap <leader>cp :!source ~/git.sh<CR>" >> ~/.vimrc
+    cp ./git.sh ~/git-auto-commit/commit.sh
+    printf "nnoremap <leader>cp :!source ~/git-auto-commit/commit.sh<CR>" >> ~/.vimrc
 else if [$input == 'neovim' || $input == 'Neovim' || $input == 'nvim' ]
 then
-    cp ./git.sh ~/.config/nvim/git.sh
-    printf "nnoremap <leader>cp :!source ~/.config/nvim/git.sh<CR>" >> ~/.config/nvim/init.vim
+    cp ./git.sh ~/.config/nvim/git-auto-commit/commit.sh
+    printf "nnoremap <leader>cp :!source ~/.config/nvim/git-auto-commit/commit.sh<CR>" >> ~/.config/nvim/init.vim
 else
     printf "You did not enter a valid option."
 fi
