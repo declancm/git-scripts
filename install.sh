7d68ec80efe8a4a51a7e77a554fca50ecd584fe2
declancm <declanmullen3.14@gmail.com>
2021-12-07 17:30:28 -0800

    auto commit @ 2021-12-07 17:30:28

--- install.sh
+++ install.sh
@@ -5,25 +5,25 @@ then
     [ ! -d ~/auto-commit ] && mkdir -p ~/auto-commit && printf "The auto-commit directory was created.\n"
     cp -f ./commit.sh ~/auto-commit/commit.sh
     printf "nnoremap <leader>cp :!source ~/auto-commit/commit.sh<CR>" > ~/auto-commit/commit-keymap.vim
-    vimMap="if filereadable(\"$HOME/auto-commit/commit-keymap.vim\")\n    source $HOME/auto-commit/commit-keymap.vim\nendif"
+    vimMap="if !empty(glob(\"\$HOME/auto-commit/commit-keymap.vim\"))\n    source \$HOME/auto-commit/commit-keymap.vim\nendif"
     if grep -qF "$vimMap" ~/.vimrc;then
         printf "The keymap is already sourced in the .vimrc\n"
     else
         printf "\n$vimMap\n" >> ~/.vimrc
     fi
-    printf "auto-commit was installed for vim\n"
+    printf "auto-commit was installed for vim. Please source your .vimrc .\n"
 elif [ "$input" = "neovim" ] || [ "$input" = "Neovim" ] || [ "$input" = "nvim" ]
 then
     [ ! -d ~/.config/nvim/auto-commit ] && mkdir -p ~/.config/nvim/auto-commit && printf "The auto-commit directory was created.\n"
     cp -f ./commit.sh ~/.config/nvim/auto-commit/commit.sh
     printf "nnoremap <leader>cp :!source ~/.config/nvim/auto-commit/commit.sh<CR>" > ~/.config/nvim/auto-commit/commit-keymap.vim
-    neovimMap="if filereadable(\"$HOME/auto-commit/commit-keymap.vim\")\n    source $HOME/.config/nvim/auto-commit/commit-keymap.vim\nendif"
+    neovimMap="if !empty(glob(\"\$HOME/.config/nvim/auto-commit/commit-keymap.vim\"))\n    source \$HOME/.config/nvim/auto-commit/commit-keymap.vim\nendif"
     if grep -qF "$neovimMap" ~/.config/nvim/init.vim;then
         printf "They keymap is already sourced in the init.vim\n"
     else
         printf "\n$neovimMap\n" >> ~/.config/nvim/init.vim
     fi
-    printf "auto-commit was installed for neovim\n"
+    printf "auto-commit was installed for neovim. Please source your init.vim .\n"
 else
     printf "You did not enter a valid option.\n"
 fi
