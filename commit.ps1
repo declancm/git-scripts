916247afb7f3e13e1d03aed3bace6e586327fcc3
declancm <declanmullen3.14@gmail.com>
2021-12-15 23:23:58 -0800

    auto commit @ 2021-12-16 07:23:58 UTC

--- commit.ps1
+++ commit.ps1
@@ -4,5 +4,12 @@
 if (&git rev-parse --git-dir | Out-Null) {
   $branch= &git rev-parse --abbrev-ref HEAD
   $gitDirectory= &git rev-parse --show-toplevel
+  Push-Location $gitDirectory
+  $remote= &git remote
+  Start-Process -Wait "git add ."
+  $dateAndTime= Get-Date -Format "
+  Start-Process -Wait "auto commit @
+
+
 }
 
